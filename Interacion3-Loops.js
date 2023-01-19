5d0eb0751cf8e4c5af9953afa02f33fec621c295
i-am-iron-man <iron-man@avengers.com>
2023-01-19 23:42:12 +0100

    completed

---
+++ Interacion3-Loops.js
@@ -0,0 +1,9 @@
+//Usa un bucle forof para recorrer todos los destinos del array. Imprime en un console.log sus valores.
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
+for (const paises of placesToTravel){
+	console.log(paises)
+}
+
+
+    
